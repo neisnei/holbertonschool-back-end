f057b2d74f1f8d8d73ed29d8348d9b2da67ea7bf
neisnei <neischalyruidiaz@gmail.com>
2023-12-21 14:54:51 +0000

    data

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,31 +3,26 @@
 Module script returns information about list progress
 """
 import requests
-import sys
-
-
-def get_employee_todo_list_progress(employee_id):
-    response = requests.get(
-        "https://api.bito.com/v1/employees/{}/todo_list".format(employee_id)
-    )
-    response.raise_for_status()
-
-    todo_list = response.json()
-
-    completed_tasks = [task for task in todo_list if task["is_done"]]
-
-    number_of_done_tasks = len(completed_tasks)
-    total_number_of_tasks = len(todo_list)
-
-    print("Employee {} is done with tasks({}/{}):".format(
-        todo_list["name"], number_of_done_tasks, total_number_of_tasks
-    ))
-
-    for task in completed_tasks:
-        print("\t{}".format(task["title"]))
+from sys import argv
 
 
 if __name__ == "__main__":
-    employee_id = int(input("Enter employee ID: "))
-
-    get_employee_todo_list_progress(employee_id)
+    if len(argv) > 1:
+        user = argv[1]
+        url = "https://jsonplaceholder.typicode.com/"
+        req = requests.get("{}users/{}".format(url, user))
+        name = req.json().get("name")
+        if name is not None:
+            jreq = requests.get(
+                "{}todos?userId={}".format(
+                    url, user)).json()
+            alltsk = len(jreq)
+            completedtsk = []
+            for t in jreq:
+                if t.get("completed") is True:
+                    completedtsk.append(t)
+            count = len(completedtsk)
+            print("Employee {} is done with tasks({}/{}):"
+                  .format(name, count, alltsk))
+            for title in completedtsk:
+                print("\t {}".format(title.get("title")))

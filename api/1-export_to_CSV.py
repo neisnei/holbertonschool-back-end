f2225bdf3251677bcdc65be8e6fab8915e9c67f0
neisnei <neischalyruidiaz@gmail.com>
2023-12-21 15:58:15 +0000

    main 0

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -24,7 +24,8 @@ def get_employee_todo_list_progress(employee_id):
     total_number_of_tasks = len(todo_list)
 
     print(
-        f"Employee {todo_list['name']} is done with tasks({number_of_done_tasks}/{total_number_of_tasks}):"
+        f"Employee {todo_list['name']} is done with tasks
+        ({number_of_done_tasks}/{total_number_of_tasks}):"
     )
 
     for task in completed_tasks:
@@ -34,11 +35,12 @@ def get_employee_todo_list_progress(employee_id):
     filename = f"{employee_id}.csv"
     with open(filename, "w", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        writer.writerow
+        (["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
         for task in todo_list:
-            writer.writerow(
-                [employee_id, todo_list["name"], task["is_done"], task["title"]]
+            writer.writerow
+            ([employee_id, todo_list["name"], task["is_done"], task["title"]]
             )
 
     print(f"Data exported to {filename}")

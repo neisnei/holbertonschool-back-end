482a1833e6137284e850f218d3260efbdc45f290
neisnei <neischalyruidiaz@gmail.com>
2023-12-21 15:04:47 +0000

    export csv

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -6,16 +6,17 @@ import requests
 from sys import argv
 
 
-if __name__ == '__main__':
-    uid = argv[1]
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(uid)
-    user = requests.get(url, verify=False).json()
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+    response = requests.get(url)
+    name = response.json().get("username")
     url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-        uid)
-    todo = requests.get(url, verify=False).json()
-    with open("{}.csv".format(uid), 'w', newline='') as csvfile:
-        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for t in todo:
-            taskwriter.writerow([int(uid), user.get('username'),
-                                 t.get('completed'),
-                                 t.get('title')])
+        argv[1])
+    response = requests.get(url)
+    tasks = response.json()
+    tasks = [{"task": task.get("title"),
+              "completed": task.get("completed"),
+              "username": name} for task in tasks]
+    data = {argv[1]: tasks}
+    with open("{}.json".format(argv[1]), "w") as jsonfile:
+        json.dump(data, jsonfile)

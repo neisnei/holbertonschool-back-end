295ec4acfd2ed59049c3a1f3a4cbaf9662847b82
neisnei <neischalyruidiaz@gmail.com>
2023-12-21 14:19:30 +0000

    export csv

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -6,46 +6,16 @@ import requests
 from sys import argv
 
 
-# Get the employee id
-employee_id = sys.argv[1]
-# Set the url and get a respnse
-user_response = requests.get(
-    f'https://jsonplaceholder.typicode.com/users/{employee_id}')
-# Get data in json form
-
-data = user_response.json()
-# Get the name of the employee
-employee_name = data['name']
-
-# Get the todo data fro the API
-todos_response = requests.get(
-    f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
-# Get the data in json form
-todos_data = todos_response.json()
-
-# Get the total number of tasks
-total_todos = len(todos_data)
-# Get the number of completed tasks
-ok_todos = sum(1 for task in todos_data if task['completed'])
-
-# Print the first line of the output
-print(
-    f'Employee {employee_name} is done with tasks({ok_todos}/{total_todos}):')
-
-# Print the title of each completed task
-for task in todos_data:
-    if task['completed']:
-        print('\t ' + task['title'])
-
-# Export using csv format
-with open('USER_ID.csv', 'w') as csvfile:
-    # Ceating a csv writer object
-    # Quoting=csv.QUOTE_ALL to quote all the fields
-    writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-    for task in todos_data:
-        # Writing the fields to the csv file
-        writer.writerow([employee_id, employee_name, task['completed'],
-                         task['title']])
-
-if __name__ == '__main__':
-    pass
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+    response = requests.get(url)
+    name = response.json().get("username")
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        argv[1])
+    response = requests.get(url)
+    tasks = response.json()
+    with open("{}.csv".format(argv[1]), "w") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            writer.writerow([argv[1], name, task.get("completed"),
+                             task.get("title")])

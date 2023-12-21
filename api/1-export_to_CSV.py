ca551d3a99bfa2ed63a182b5c12254e1b0f9a161
neisnei <neischalyruidiaz@gmail.com>
2023-12-21 15:23:40 +0000

    export csv

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -6,17 +6,40 @@ import requests
 from sys import argv
 
 
+def get_employee_todo_list_progress(employee_id):
+    response = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
+    )
+    response.raise_for_status()
+    todo_list = response.json()
+
+    # Get user info
+    user_response = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    )
+    user_response.raise_for_status()
+    user_info = user_response.json()
+
+    # Extract relevant information
+    user_id = user_info["id"]
+    username = user_info["username"]
+
+    # Export data to CSV
+    filename = "{}.csv".format(user_id)
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+
+        for task in todo_list:
+            writer.writerow([user_id, username, task["completed"], task["title"]])
+
+    print("Data exported to {}".format(filename))
+
+
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
-    response = requests.get(url)
-    name = response.json().get("username")
-    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-        argv[1])
-    response = requests.get(url)
-    tasks = response.json()
-    tasks = [{"task": task.get("title"),
-              "completed": task.get("completed"),
-              "username": name} for task in tasks]
-    data = {argv[1]: tasks}
-    with open("{}.json".format(argv[1]), "w") as jsonfile:
-        json.dump(data, jsonfile)
+    if len(sys.argv) < 2:
+        print("Please provide an employee ID.")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    get_employee_todo_list_progress(employee_id)

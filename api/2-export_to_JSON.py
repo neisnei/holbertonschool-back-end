c60f2cceb4e7ec93e79c0a1178ae7f2df91cd5fc
neisnei <neischalyruidiaz@gmail.com>
2023-12-22 18:08:51 +0000

    export data json

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-'''
-Module script to export data in the JSON format.
-'''
-
-import json
-import requests
-from sys import argv
+"""Script to export data i  the JSON format"""
 
 
-if __name__ == '__main__':
-    uid = argv[1]
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(uid)
-    user = requests.get(url, verify=False).json()
-    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(uid)
-    todo = requests.get(url, verify=False).json()
-    name = user.get('username')
-    t = [{"task": t.get("title"),
-          "username": name,
-          "completed": t.get("completed")} for t in todo]
-    bj = {}
-    bj[uid] = t
-    with open("{}.json".format(uid), 'w') as filejs:
-        json.dump(bj, filejs)
+if __name__ == "__main__":
+    import json
+    import requests
+    from sys import argv
+
+    u_id = argv[1]
+    api_url = "https://jsonplaceholder.typicode.com/users/{}".format(u_id)
+    api_url2 = "https://jsonplaceholder.typicode.com/todos?userId={}"\
+        .format(u_id)
+    response = requests.get(api_url).json()
+    EMPLOYEE_NAME = response.get('username')
+    response = requests.get(api_url2).json()
+    f_name = u_id + '.json'
+    u_list = {u_id: []}
+    for info in response:
+        dic = {"task": info.get('title'), "completed": info.get('completed'),
+               "username": EMPLOYEE_NAME}
+        u_list.get(u_id).append(dic)
+    with open(f_name, 'w', encoding='utf-8') as f:
+        json.dump(u_list, f)
